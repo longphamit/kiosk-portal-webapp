2ed97f001b892bd86c9a911d372b8b1019347907
hhtin <tinhuynhhuu250500@gmail.com>
2022-08-01 17:28:15 +0700

    CPF_339

--- src/kiosk_portal/pages/app_category/formCreate.jsx
+++ src/kiosk_portal/pages/app_category/formCreate.jsx
@@ -1,28 +1,34 @@
-import { Button, Form, Input, Upload } from "antd";
+import { Button, Form, Input, Spin, Upload } from "antd";
 import { formItemLayout, tailFormItemLayout } from "../../layouts/form_layout";
 import { UploadOutlined } from "@ant-design/icons";
 import { beforeUpload } from "../../../@app/utils/image_util";
 import { getBase64 } from "../../../@app/utils/file_util";
 import { createCategoriesService } from "../../services/categories_service";
 import { toast } from "react-toastify";
+import { useState } from "react";
 
 const FormCreateCategory = ({ visible }) => {
   const [form] = Form.useForm();
+  const [isLoading, setIsLoading] = useState(false);
   const onFinishCreateCategories = async (values) => {
-    let formatResult = [];
-    const result = await getBase64(values.logo.file.originFileObj);
-    formatResult = result.split(",");
-    const newCategories = {
-      name: values.name,
-      logo: formatResult[1],
-    };
     try {
+      setIsLoading(true);
+      let formatResult = [];
+      const result = await getBase64(values.logo.file.originFileObj);
+      formatResult = result.split(",");
+      const newCategories = {
+        name: values.name,
+        logo: formatResult[1],
+        commissionPercentage: parseInt(values.commissionPercentage, 10),
+      };
       await createCategoriesService(newCategories);
       toast.success("Create Category Success");
       visible(false);
       form.resetFields();
     } catch (error) {
       console.error(error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -67,10 +73,33 @@ const FormCreateCategory = ({ visible }) => {
             <Button icon={<UploadOutlined />}>Upload</Button>
           </Upload>
         </Form.Item>
+
+        <Form.Item
+          name="commissionPercentage"
+          label="Commission"
+          rules={[
+            {
+              required: true,
+              message: "Please input Commission Percentage!",
+            },
+            {
+              pattern: "^[0-9]$|^[1-9][0-9]$|^(100)$",
+              message: "Please input number >0 and <100",
+            },
+          ]}
+        >
+          <Input
+            addonAfter={<p style={{ height: 16, fontWeight: 900 }}>%</p>}
+          />
+        </Form.Item>
         <Form.Item {...tailFormItemLayout}>
-          <Button type="primary" htmlType="submit">
-            Create Application
-          </Button>
+          {isLoading ? (
+            <Spin />
+          ) : (
+            <Button type="primary" htmlType="submit">
+              Create Application
+            </Button>
+          )}
         </Form.Item>
       </Form>
     </>
